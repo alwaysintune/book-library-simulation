82a4c11ee98b2529f641c01f075f9459ecab33df
alwaysintune <domantas.slenys@gmail.com>
2021-06-16 19:11:47 +0300

    covering add book functionality with unit tests

--- tests/BookLibrary.UnitTests/ServicesTests/LibraryServiceTest.cs
+++ tests/BookLibrary.UnitTests/ServicesTests/LibraryServiceTest.cs
@@ -1,6 +1,8 @@
 ﻿using BookLibrary.ConsoleApp.Entities;
 using BookLibrary.ConsoleApp.Services.Library;
 using System;
+using System.Collections.Generic;
+using Xunit;
 
 namespace BookLibrary.UnitTests.ServicesTests
 {
@@ -42,6 +44,45 @@ namespace BookLibrary.UnitTests.ServicesTests
 
             var address = new Address("_street", "_zipcode");
             _libraryCard = new LibraryCard("_fullname", address);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void AddBook_ThrowsArgumentException_WhenCountBelowOne(int count)
+        {
+            void act() => _libraryService.AddBook(_book, count);
+
+            ArgumentException exception = Assert.Throws<ArgumentException>(act);
+            Assert.Equal("count", exception.ParamName);
+        }
+
+        [Fact]
+        public void AddBook_ThrowsArgumentException_WhenDifferentBookWithExistingISBN()
+        {
+            var book2 = _book with { Name = "_name2" };
+
+            _libraryService.AddBook(_book, 1);
+            void act() => _libraryService.AddBook(book2, 1);
+
+            ArgumentException exception = Assert.Throws<ArgumentException>(act);
+            Assert.Equal("book", exception.ParamName);
+        }
+
+        [Fact]
+        public void AddBook_IncreasesAvailableCount_WhenBookAlreadyExists()
+        {
+            int count = 1;
+            int repeat = 2;
+
+            for (int i = 0; i < repeat; i++)
+            {
+                _libraryService.AddBook(_book, count);
+            }
+            List<LibraryBook> books = _libraryService.ListAllBooks();
+
+            Assert.Single(books);
+            Assert.Equal(repeat * count, books[0].Inventory.AvailableCount);
         }
     }
 }
