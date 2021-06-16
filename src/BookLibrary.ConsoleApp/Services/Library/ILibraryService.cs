a68ac93f2bda960c7e9a3808acb528fdfe7dc2ac
alwaysintune <domantas.slenys@gmail.com>
2021-06-16 18:37:32 +0300

    adding library's add book functionality

---
+++ src/BookLibrary.ConsoleApp/Services/Library/ILibraryService.cs
@@ -0,0 +1,9 @@
+﻿using BookLibrary.ConsoleApp.Entities;
+
+namespace BookLibrary.ConsoleApp.Services.Library
+{
+    public interface ILibraryService
+    {
+        void AddBook(Book book, int count);
+    }
+}
---
+++ src/BookLibrary.ConsoleApp/Services/Library/LibraryService.cs
@@ -0,0 +1,59 @@
+﻿using BookLibrary.ConsoleApp.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace BookLibrary.ConsoleApp.Services.Library
+{
+    public class LibraryService : ILibraryService
+    {
+        public readonly Dictionary<string, LibraryBook> LibraryBooks;
+
+        public LibraryService()
+        {
+            LibraryBooks = new Dictionary<string, LibraryBook>();
+        }
+
+        /// <summary>
+        /// If book already exists, only increases availability count
+        /// </summary>
+        /// <param name="book"></param>
+        /// <param name="count"></param>
+        public void AddBook(Book book, int count)
+        {
+            if (count <= 0)
+            {
+                throw new ArgumentException(
+                    "Count may not be equal to or less than zero",
+                    paramName: nameof(count)
+                    );
+            }
+
+            if (LibraryBooks.ContainsKey(book.ISBN))
+            {
+                if (LibraryBooks[book.ISBN].Book == book) // record value equality
+                {
+                    LibraryBooks[book.ISBN].Inventory.AvailableCount += count;
+                }
+                else
+                {
+                    throw new ArgumentException(
+                        "Trying to add a different book with existing ISBN",
+                        paramName: nameof(book));
+                }
+            }
+            else
+            {
+                LibraryBooks.Add(
+                    book.ISBN,
+                    new LibraryBook(
+                        book,
+                        new BookInventory
+                        {
+                            TakenCount = 0,
+                            AvailableCount = count
+                        })
+                    );
+            }
+        }
+    }
+}
